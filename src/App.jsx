a788b9d19410dbc670a7807676b3fabefcdf4fd6
Willigers <ronowilligers@gmail.com>
2024-11-02 14:06:12 +0300

    Made changes on navbar file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "./components/Navbar/Navbar";
+import NavBar from "./components/Navbar/NavBar.jsx";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Technology from "./components/Technology/Technology";
